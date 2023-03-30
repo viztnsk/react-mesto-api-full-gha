82b7f68025c0e696acc22701a93442e71aa4143c
Victoria Zatonskaya <viztnsk@yandex.ru>
2023-03-30 17:16:51 +0300

    added crash test

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,12 @@ app.use('*', cors());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+}); 
+
 app.post('/signin', loginValidation, login);
 app.post('/signup', registerValidation, createUser);
 
