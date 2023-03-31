c5b41748adc4df28513edb62b09bf60285308bbd
Victoria Zatonskaya <viztnsk@yandex.ru>
2023-03-31 14:36:39 +0300

    edited back port 3000

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const NODE_ENV = process.env;
 const { JWT_SECRET = '569856448aaa06706bdcdff06b9e1bc71622d0af38f58d5f617eb6d6c1f1359b' } = process.env;
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 module.exports = {
